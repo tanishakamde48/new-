aba37b273263261f21f5b41678fdeee0b43cfda0
tanishakamde48 <tanishakamde48@gmail.com>
2026-08-14 12:47:12 +0530

    ok

---
+++ PythonProject/basic/helloworld.py
@@ -0,0 +1,73 @@
+# # # # print("hello world")
+# # # # print("tanisha")
+# # # # nums = {1,5,6,3,7,9,5,6,1,55,55}
+# # # # print(len(nums))
+# # # class = [
+# # #     "name" : "tannisha",
+# # #     "class": 10,
+# # #     "marks" : {
+# # #     "phy" : 56,
+# # #     "chem": 58,
+# # #     "maths": 45,
+# # # }
+# # # ]
+# # # print(class["name"])
+# # dict = {
+# #     "name": "tanisha",
+# #     "age" : "21",
+# #     "marks":{
+# #         "phy": 55,
+# #         "chemistry": 56,
+# #         "maths": "59"
+# #     }
+# # }
+# # print(dict["name"])
+# # print(dict["age"])
+# # print(dict["marks"])
+# # print("")
+#
+# set = {55,55,55,55,65,75,65,85,95,100,100}
+# print(set)
+# print((type(set)))
+# print(len(set))
+# a = 10
+# b = 20
+# if a < b:
+#     print("a  is less than b :")
+# else:
+#     print("a is greater than ")
+# i = 0
+# while i <= 100:
+#     print(i)
+#     i += 1
+# n = int(input("enter a number"))
+# if number % 2 == 0:
+# print(n,"is even number")
+# else:
+# print(n, "is odd number")
+# number = int(input("enter a number"))
+#
+# if number % 2 == 0:
+#     print(number,"is even number")
+# else:
+#     print(number,"is odd number")
+# n = 10
+# if n%2 == 0:
+#         print(n, "is even")
+# else:
+# #         print(n,"is odd")
+# i = int(input("enter a number"))
+# for i in range(1,101):
+#     if i % 7 ==0:
+#         print(i,("is divisible by 7"))
+# for i in range(1,100):
+#     if i%7==0:
+#         print(i,"divisible by 7")
+# i = 100
+# while i>=1:
+#     print(i)
+#     i-=1
+a = 10
+b = 20
+if a>b:
+    print("a is greater than b")
