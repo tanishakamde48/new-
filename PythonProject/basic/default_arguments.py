23831bcdaf0f5f08e9e4729567d1b8a6a87f82cd
tanishakamde48 <tanishakamde48@gmail.com>
2026-08-27 21:02:28 +0530

    ok

---
+++ PythonProject/basic/default_arguments.py
@@ -0,0 +1,9 @@
+def student(name,city="nagpur"):
+    print(name,"lives in ",city)
+s = student
+s("tanisha")
+s("palak","indore")
+s("gitanjali","indore")
+s("priyanka")
+s("rahul")
+s("priyanshu","bhopal")
