d3e7bf3985abe6e4f6782acf805f01780ead6b9f
tanishakamde48 <tanishakamde48@gmail.com>
2026-08-28 12:57:53 +0530

    return statement

---
+++ PythonProject/basic/return statement.py
@@ -0,0 +1,22 @@
+def sum(a,b):
+    return a+b
+result = sum(50,50)
+print(result)
+
+
+def hello(a,b):
+    return a*b
+result = hello(2,6)
+print(result)
+
+
+def obj(a,b):
+    return a-b
+result = obj(50,5)
+print(result)
+
+def div(sum):
+    return sum/10
+result = div(50)
+print(result)
+
