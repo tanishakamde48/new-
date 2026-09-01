eef82a7798174fadf4fd1c1042e3efb7f9f58415
tanishakamde48 <tanishakamde48@gmail.com>
2026-09-01 14:32:55 +0530

    local&global variable

---
+++ PythonProject/basic/local & global variable.py
@@ -0,0 +1,32 @@
+
+# local variable
+
+def demo():
+    x = 10
+    y = 20
+    print(x+y)
+demo()
+
+
+# local variable
+
+def demo():
+    x = 10
+    y = 20
+    print(x-y)
+demo()
+
+
+# global variable
+
+college = "tanisha kamde"
+
+def show():
+    print(college)
+def display():
+    print(college)
+def hello():
+    print(college)
+show()
+display()
+hello()
