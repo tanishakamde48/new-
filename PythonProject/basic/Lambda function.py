6e240e6a5cc29cec000cac0d6e655b9936728aef
tanishakamde48 <tanishakamde48@gmail.com>
2026-09-01 15:01:58 +0530

    lambda_function

---
+++ PythonProject/basic/Lambda function.py
@@ -0,0 +1,35 @@
+# normal function
+
+def square (x):
+    return x*x
+print(square(4))
+
+def square (x):
+    return x*x
+print(square(5))
+
+def square (x):
+    return x*x
+print(square(8))
+
+def square (x):
+    return x*x
+print(square(7))
+
+# lambda function
+# small one line function
+square = lambda x:x*x
+print(square(5))
+
+tanisha = lambda x:x*x
+print(tanisha(4))
+
+hello = lambda x: x-x
+print(hello(5))
+
+sum = lambda x: x+x
+print(sum(4))
+
+
+sum = lambda y: y+y
+print(sum(50))
